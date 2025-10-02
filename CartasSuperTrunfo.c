21de763df6939733f1ccaf8285e5659118e09775
heytorcode88 <heybar17@hotmail.com>
2025-10-02 02:12:36 +0000

    adição de PIB per capita e densidade populacional

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,8 @@ int main(){
     float area1 = 50.000;
     float pib1 = 95.000000;
     int turisticos1 = 40;
+    float densidade1, pibpercapita1; 
+
 
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
@@ -18,6 +20,13 @@ int main(){
     printf("PIB: %f\n", pib1);
     printf("Pontos túristicos: %d\n", turisticos1);
 
+    densidade1 = (float) populacao1 / area1;
+    printf("Densidade Populacional: %f\n", densidade1);
+
+    pibpercapita1 = (float) pib1 / populacao1;
+    printf("PIB per Capita: %f\n", pibpercapita1);
+
+
 
     char estado2 = 'B';
     char codigo2[5] = "B02";
@@ -26,7 +35,8 @@ int main(){
     float area2 = 60.000;
     float pib2 = 950.000000;
     int turisticos2 = 50;
-    
+    float densidade2, pibpercapita2;
+
 
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
@@ -37,10 +47,17 @@ int main(){
     printf("Pontos túristicos: %d\n", turisticos2);
 
 
+    densidade2 = (float) populacao2 / area2;
+    printf("Densidade Populacional: %f\n", densidade1);
+
+    pibpercapita2 = (float) pib2 / populacao2;
+    printf("PIB per Capita: %f\n", pibpercapita2);
 
 
 
 
+
+    return 0;
 
 
 }
